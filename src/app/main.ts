2bf07198400238ebb26b171e839227186520eb88
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-18 17:48:27 -0400

    interface complex

---
+++ src/app/main.ts
@@ -0,0 +1,13 @@
+import { addProduct } from "./products/product.service";
+
+addProduct({
+  id: '1',
+  title: 'p1',
+  createdAt: new Date(),
+  stock: 12,
+  size: "S",
+  category: {
+    id: '13',
+    name: 'c1'
+  }
+})
